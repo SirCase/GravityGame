7a579ef7f78e2a4d4cce5589eacd9c247ebb705e
Vanillabeandean <deanbarkin@gmail.com>
2019-10-30 11:46:17 -0700

    changed stuff

--- player.js
+++ player.js
@@ -1,53 +1,12 @@
 //Create the player and it's movements
 
 function InitializeCharacter (){
-var playerImage = new Image ();
-playerImage.src = "Sprites/player.png"
 
-}
-function sprite (options) {
-
-    var that = {};
-
-    that.context = options.context;
-    that.width = options.width;
-    that.height = options.height;
-    that.image = options.image;
-
-    return that;
-  }
-  var canvas = document.getElementById('map.js');
+  var canvas = document.getElementById('mainCanvas');
     var context = canvas.getContext('2d');
     var background = new Image();
-  background.onload = function() {
-      context.drawImage(img,0,0,600,300);
-    }
   background.src = 'Sprites/background.gif';
 
-
-    var player = sprite({
-        context: canvas.getContext("2d"),
-        width: 100,
-        height: 100,
-        image: playerImage
-    });
-    function sprite (options) {
-
-
-    that.render = function () {
-
-        // Draw the animation
-        that.context.drawImage(
-           that.image,
-           0,
-           0,
-           that.width,
-           that.height,
-           0,
-           0,
-           that.width,
-           that.height);
-    };
-
-
+  //background.onload = function() {
+      context.drawImage(background,0,0,600,300);
 }
