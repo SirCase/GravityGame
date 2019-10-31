c69c496e4954fbf30232239210fca0972aeb6f12
Vanillabeandean <deanbarkin@gmail.com>
2019-10-31 11:50:42 -0700

    Trying to get background image in game

--- map.js
+++ map.js
@@ -20,3 +20,16 @@
 //   x : 0,
 //   y : 0
 // };
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 300;
+
+
+var background = new Image();
+background.src = "Sprites/background.gif";
+
+background.onload = function(){
+    ctx.drawImage(background,0,0);
+}
