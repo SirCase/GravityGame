ed1e59b9db77bbb0e503ee1755dfa1fa0bcb0b94
Vanillabeandean <deanbarkin@gmail.com>
2019-10-30 23:54:04 -0700

    Days 1-3 sort of

--- animate.js
+++ animate.js
@@ -1,14 +1,14 @@
-function handlePlayerAnimation() {
-  if (CONTROLS.player.up) {
-    PLAYER.y += 10;
-  }
-  if (CONTROLS.player.down) {
-    PLAYER.y -=  5
-  }
-  if (CONTROLS.player.left) {
-    PLAYER.x -= 3;
-  }
-  if (CONTROLS.player.right) {
-    PLAYER.x += 3;
-  }
-}
+// function handlePlayerAnimation() {
+//   if (CONTROLS.player.up) {
+//     PLAYER.y += 10;
+//   }
+//   if (CONTROLS.player.down) {
+//     PLAYER.y -=  5
+//   }
+//   if (CONTROLS.player.left) {
+//     PLAYER.x -= 3;
+//   }
+//   if (CONTROLS.player.right) {
+//     PLAYER.x += 3;
+//   }
+// }
--- player.js
+++ player.js
@@ -1,12 +1,10 @@
-//Create the player and it's movements
-
-function InitializeCharacter (){
-
-  var canvas = document.getElementById('mainCanvas');
-    var context = canvas.getContext('2d');
-    var background = new Image();
-  background.src = 'Sprites/background.gif';
-
-  //background.onload = function() {
-      context.drawImage(background,0,0,600,300);
-}
+// //Create the player and it's movements
+//
+// function InitializeCharacter (){
+//
+//
+//     var background = new Image(600,300);
+//   background.src = 'Sprites/background.gif';
+//
+//   
+// //}
